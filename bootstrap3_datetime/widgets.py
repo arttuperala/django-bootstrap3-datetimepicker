a23a45088383f0f8e003f24b0b16f82d2629c4b5
Tommi Kaikkonen <kaikkonentommi@gmail.com>
2018-03-09 14:26:14 -0800

    Fix compatibility for build_attrs changed in Django 1.11

--- bootstrap3_datetime/widgets.py
+++ bootstrap3_datetime/widgets.py
@@ -124,7 +124,11 @@ class DateTimePicker(DateTimeInput):
     def render(self, name, value, attrs=None):
         if value is None:
             value = ''
-        input_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
+
+        base_attrs = self.attrs.copy()
+        base_attrs.update({'type': self.input_type, 'name': name})
+        input_attrs = self.build_attrs(base_attrs, extra_attrs=attrs)
+
         if value != '':
             # Only add the 'value' attribute if a value is non-empty.
             input_attrs['value'] = force_text(self._format_value(value))
